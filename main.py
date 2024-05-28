ba8f3c9e3548187750ed0581a6dd847590dcf2e0
JaacobK <140124151+JaacobK@users.noreply.github.com>
2024-05-28 14:52:47 +1000

    done

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from shapes import Shape, Rectangle, Circle
 
-rectangle = Rectangle(12, 5)
-print(rectangle.area())
-print(rectangle.perimiter())
+print("Welcome to Shapes and Geometry")
+choice = input("type\n1. For rectangle\n2. For circle\n")
+if choice == "1":
+    width = int(input("Please enter the width of the rectangle\n"))
+    height = int(input("Please enter the hieght of the rectangle\n"))
+    rectangle = Rectangle(width, height)
+    print(f"Your rectangles area is {rectangle.area()}\n And your rectangles perimiter is {rectangle.perimiter()}")
+
+if choice == "2":
+    radius = int(input("Please enter the radius of the circle\n"))
+    circle = Circle(radius)
+    print(f"Your circle area is {circle.area()}\n And your circle perimiter is {circle.perimiter()}")
